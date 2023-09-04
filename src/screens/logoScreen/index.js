42b57fb3cd5ed0c8c753ae9e3189a61577045573
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-09-04 12:22:10 -0300

    config for eslint and prettier

--- src/screens/logoScreen/index.js
+++ src/screens/logoScreen/index.js
@@ -1,22 +1,22 @@
-import { StyleSheet, View, StatusBar } from "react-native";
-import { CustomText } from "../../components/atoms/text";
-import { Logo } from "../../components/atoms/logo";
+import { StyleSheet, View, StatusBar } from 'react-native'
+import { CustomText } from '../../components/atoms/text'
+import { Logo } from '../../components/atoms/logo'
 
 export const LogoScreen = () => {
   return (
     <View style={styles.container}>
       <Logo />
-      <CustomText>Star wars - wiki </CustomText>
+      <CustomText>Starwars - Wiki </CustomText>
       <StatusBar style="auto" />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#161616",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#161616',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
